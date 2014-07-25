f15504a10ead7c6a1c3241c2a7cc94c6b8d0c8a0
Corbin Uselton <corbinu@openswimsoftware.com>
2014-07-25 14:37:54 -0400

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,8 +15,16 @@ module.exports = function(grunt) {
 				command: 'ant update-all',
 				cwd: 'build/ceylon-dist'
 			},
+			disteclipse: {
+				command: 'ant clean publish-all ide-quick',
+				cwd: 'build/ceylon-dist'
+			},
+			distpermiss: {
+				command: 'sudo chmod -R 777 dist',
+				cwd: 'out'
+			},
 			eclipsebuild: {
-				command: 'mvn clean install -Dmaven.test.skip',
+				command: 'mvn clean install -DskipTests',
 				cwd: 'build/ceylon-ide-eclipse'
 			},
 			eclipseupdate: {
@@ -27,9 +35,21 @@ module.exports = function(grunt) {
 				command: 'ant publish',
 				cwd: 'build/ceylon-sdk'
 			},
+			sdkeclipse: {
+				command: 'ant clean publish ide-quick',
+				cwd: 'build/ceylon-sdk'
+			},
 			sdkupdate: {
 				command: 'git pull',
 				cwd: 'build/ceylon-sdk'
+			},
+			formatterbuild: {
+				command: 'ant clean publish ide-quick',
+				cwd: 'build/ceylon.formatter'
+			},
+			formatterupdate: {
+				command: 'git pull',
+				cwd: 'build/ceylon.formatter'
 			}
 		},
 		gitclone: {
@@ -43,6 +63,12 @@ module.exports = function(grunt) {
 				options: {
 					repository: 'https://github.com/ceylon/ceylon-sdk.git',
 					directory: 'build/ceylon-sdk'
+				}
+			},
+			formatter: {
+				options: {
+					repository: 'https://github.com/ceylon/ceylon.formatter.git',
+					directory: 'build/ceylon.formatter'
 				}
 			},
 			eclipse: {
@@ -103,14 +129,7 @@ module.exports = function(grunt) {
 			}
 		},
 		clean: {
-			dist: [
-				'build/ceylon-dist', 'build/ceylon-common',
-				'build/ceylon-module-resolver', 'build/ceylon-spec',
-				'build/ceylon-compiler', 'build/ceylon-js',
-				'build/ceylon.language', 'build/ceylon-runtime'
-			],
-			sdk: [ 'build/ceylon-sdk' ],
-			eclipse: [ 'build/ceylon-ide-eclipse' ],
+			dist: [ 'build'	],
 			out: [ 'out' ]
 		}
 	});
@@ -122,9 +141,9 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('default', ['update', 'build']);
 
-	grunt.registerTask('setup', ['gitclone:dist', 'gitclone:sdk', 'gitclone:eclipse', 'exec:distsetup']);
-	grunt.registerTask('update', ['exec:distupdate', 'exec:sdkupdate', 'exec:eclipseupdate']);
+	grunt.registerTask('setup', ['gitclone:dist', 'gitclone:sdk', 'gitclone:formatter', 'gitclone:eclipse', 'exec:distsetup']);
+	grunt.registerTask('update', ['exec:distupdate', 'exec:sdkupdate', 'exec:eclipseupdate', 'exec:formatterupdate']);
 
-	grunt.registerTask('build', ['clean:out', 'exec:distpublish', 'exec:eclipsebuild', 'exec:sdkbuild', 'copy']);
+	grunt.registerTask('build', ['clean:out', 'exec:distpublish', 'exec:sdkbuild', 'exec:formatterbuild', 'exec:disteclipse', 'exec:sdkeclipse', 'exec:eclipsebuild', 'copy', 'exec:distpermiss']);
 
 };
